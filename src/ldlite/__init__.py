1868a0918afb26a4c7b449168d559b510772b2a5
Nassib Nassar <nrn@etymon.com>
2021-09-18 23:50:25 -0400

    Add docstrings

--- src/ldlite/__init__.py
+++ src/ldlite/__init__.py
@@ -6,7 +6,7 @@ import requests
 from tabulate import tabulate
 from tqdm import tqdm
 
-def escape_sql(sql):
+def _escape_sql(sql):
     n = ""
     for c in sql:
         if c == "'":
@@ -17,29 +17,34 @@ def escape_sql(sql):
 
 class LDLite:
 
-    def __init__(self, debug=False, quiet=False):
-        if debug and quiet:
-            raise ValueError("parameters \"debug\" and \"quiet\" are mutually exclusive")
+    def __init__(self):
+        """Creates an instance of LDLite."""
         self.page_size = 1000
-        self.debug = debug
-        self.quiet = quiet
-
-    def close(self):
-        pass
+        self.debug = False
+        self.quiet = False
 
     def _set_page_size(self, page_size):
         self.page_size = page_size
 
     def config_okapi(self, url, tenant, user, password):
+        """Configures the connection to Okapi.
+
+        The *url*, *tenant*, *user*, and *password* settings are Okapi-specific
+        connection parameters.
+        """
         self.okapi_url = url
         self.okapi_tenant = tenant
         self.okapi_user = user
         self.okapi_password = password
 
     def config_db(self, db):
+        """Configures the analytic database.
+
+        The *db* parameter must be an existing database connection.
+        """
         self.db = db
 
-    def login(self):
+    def _login(self):
         if self.debug:
             print("ldlite: logging in to okapi", file=sys.stderr)
         hdr = { 'X-Okapi-Tenant': self.okapi_tenant,
@@ -49,7 +54,7 @@ class LDLite:
         resp = requests.post(self.okapi_url+'/authn/login', headers=hdr, data=json.dumps(data))
         return resp.headers['x-okapi-token']
 
-    def transform_json(self, table, total):
+    def _transform_json(self, table, total):
         cur = self.db.cursor()
         cur.execute("SELECT jsonb FROM "+table)
         attrset = set()
@@ -84,7 +89,7 @@ class LDLite:
             values = []
             for a in d.keys():
                 attrs.append("\""+a+"\"")
-                values.append("'"+escape_sql(str(d[a]))+"'")
+                values.append("'"+_escape_sql(str(d[a]))+"'")
             if len(attrs) != 0:
                 q = "INSERT INTO \""+table+"_j\" ("
                 q += "__id,"
@@ -100,8 +105,15 @@ class LDLite:
         if not self.quiet:
             pbar.close()
 
-    def query(self, table, path, query, transform=True):
-        token = self.login()
+    def query(self, table: str, path: str, query: str, transform: str =True):
+        """Submits a CQL query to an Okapi module.
+
+        The *path* parameter is the request path, and *query* is the CQL query.
+        The result is stored in *table* within the analytic database.  If
+        *transform* is True, JSON data are transformed into one or more tables
+        that are created in addition to *table*.
+        """
+        token = self._login()
         self.db.execute("DROP TABLE IF EXISTS "+table)
         self.db.execute("CREATE TABLE "+table+"(__id integer, jsonb varchar)")
         hdr = { 'X-Okapi-Tenant': self.okapi_tenant,
@@ -131,7 +143,7 @@ class LDLite:
             if lendata == 0:
                 break
             for d in data:
-                self.db.execute("INSERT INTO "+table+" VALUES ("+str(count+1)+", '"+escape_sql(json.dumps(d, indent=4))+"')")
+                self.db.execute("INSERT INTO "+table+" VALUES ("+str(count+1)+", '"+_escape_sql(json.dumps(d, indent=4))+"')")
                 count += 1
                 if not self.quiet:
                     if pbartotal + 1 > total:
@@ -146,9 +158,13 @@ class LDLite:
         if transform:
             if not self.quiet:
                 print("ldlite: transforming data", file=sys.stderr)
-            self.transform_json(table, count)
+            self._transform_json(table, count)
 
-    def select(self, table, limit):
+    def select(self, table: str, limit: int):
+        """Prints rows of a table in the analytic database.
+
+        Up to *limit* rows of *table* are printed to standard output.
+        """
         q = "SELECT * FROM \""+table+"\" LIMIT "+str(limit)
         self.db.execute(q)
         hdr = []
@@ -158,7 +174,11 @@ class LDLite:
             print("ldlite: reading from table: "+table, file=sys.stderr)
         print(tabulate(self.db.fetchall(), headers=hdr, tablefmt='fancy_grid'))
 
-    def to_csv(self, filename, table, limit):
+    def to_csv(self, filename: str, table: str, limit: int):
+        """Export a table in the analytic database to a CSV file.
+
+        Up to *limit* rows of *table* are exported to *filename* in CSV format.
+        """
         q = "SELECT * FROM \""+table+"\" LIMIT "+str(limit)
         if self.debug:
             print("ldlite: reading from table: "+table, file=sys.stderr)
@@ -167,8 +187,25 @@ class LDLite:
             print("ldlite: exporting CSV to file: "+filename, file=sys.stderr)
         df.to_csv(filename, encoding="utf-8", index=False)
 
-def init(debug=False, quiet=False):
-    return LDLite(debug, quiet)
+    def set_debug(self, enable: bool):
+        """Configures debugging output.
+
+        If *enable* is True, debugging output is enabled; otherwise it is
+        disabled.
+        """
+        if enable and self.quiet:
+            raise ValueError("\"debug\" and \"quiet\" modes cannot both be enabled")
+        self.debug = enable
+
+    def set_quiet(self, enable: bool):
+        """Configures suppression of progress output.
+
+        If *enable* is True, progress output is suppressed; otherwise it is not
+        suppressed.
+        """
+        if enable and self.debug:
+            raise ValueError("\"debug\" and \"quiet\" modes cannot both be enabled")
+        self.quiet = enable
 
 if __name__ == '__main__':
     pass
