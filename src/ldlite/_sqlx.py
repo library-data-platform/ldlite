00807666e2a07eaa194f2c1d07cd48a9fb1e15cd
Nassib Nassar <nrn@etymon.com>
2021-09-22 06:39:08 -0400

    Fix csv escaping bug

--- src/ldlite/_sqlx.py
+++ src/ldlite/_sqlx.py
@@ -10,11 +10,11 @@ def _sqlid(ident):
         return '.'.join(['"'+s+'"' for s in sp])
 
 def _escape_sql(sql):
-    n = ''
+    b = ''
     for c in sql:
         if c == '\'':
-            n += '\'\''
+            b += '\'\''
         else:
-            n += c
-    return n
+            b += c
+    return b
 
