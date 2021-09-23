31836d75f14413a2f69d62be6555fa81dc279e06
Nassib Nassar <nrn@etymon.com>
2021-09-22 21:52:40 -0400

    Continue snapshot example after error

--- examples/snapshot.py
+++ examples/snapshot.py
@@ -1,5 +1,6 @@
 # This script uses LDLite to extract sample data from folio-snapshot.
 
+import traceback
 import ldlite
 ld = ldlite.LDLite()
 ld.connect_okapi(url='https://folio-snapshot-okapi.dev.folio.org',
@@ -134,7 +135,11 @@ queries = [
 
 tables = []
 for q in queries:
-    tables += ld.query(table=q[0], path=q[1], query=q[2])
+    try:
+        t = ld.query(table=q[0], path=q[1], query=q[2])
+    except Exception as e:
+        traceback.print_exception(type(e), e, e.__traceback__)
+    tables += t
 print()
 print('Tables:')
 for t in tables:
