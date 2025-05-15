d12dcc86b77c77ad0b0a73db4b99f6636a4e3d75
Katherine Bargar <kbargar@fivecolleges.edu>
2025-05-15 18:10:17 +0000

    Run ruff format at _camelcase.py

--- src/ldlite/_camelcase.py
+++ src/ldlite/_camelcase.py
@@ -4,7 +4,7 @@ def _decode_triple(c1, c2, c3):
     Examines a sequence of three characters c1, c2, and c3; decodes c2; and
     returns the decoded output.
     """
-    b = ''
+    b = ""
     c1u = c1.isupper()
     c2u = c2.isupper()
     c3u = c3.isupper()
@@ -13,7 +13,7 @@ def _decode_triple(c1, c2, c3):
     # First check triples that include zeros
     if c2 == chr(0):
         return b
-    elif c1 == chr(0) and c2 != chr(0):
+    if c1 == chr(0) and c2 != chr(0):
         write = c2.lower()
     elif c1 != chr(0) and c2 != chr(0) and c3 == chr(0):
         if not c1u and c2u:
@@ -21,26 +21,12 @@ def _decode_triple(c1, c2, c3):
             write = c2.lower()
         elif c1u and c2u:
             write = c2.lower()
-        elif not c1u and not c2u:
-            write = c2
-        elif c1u and not c2u:
+        elif (not c1u and not c2u) or (c1u and not c2u):
             write = c2
     # Check triples having no zeros
-    elif not c1u and not c2u and not c3u:
+    elif (not c1u and not c2u and not c3u) or (c1u and not c2u and not c3u) or (not c1u and not c2u and c3u) or (c1u and not c2u and c3u):
         write = c2
-    elif c1u and not c2u and not c3u:
-        write = c2
-    elif not c1u and not c2u and c3u:
-        write = c2
-    elif c1u and not c2u and c3u:
-        write = c2
-    elif not c1u and c2u and not c3u:
-        write_break = True
-        write = c2.lower()
-    elif c1u and c2u and not c3u:
-        write_break = True
-        write = c2.lower()
-    elif not c1u and c2u and c3u:
+    elif (not c1u and c2u and not c3u) or (c1u and c2u and not c3u) or (not c1u and c2u and c3u):
         write_break = True
         write = c2.lower()
     elif c1u and c2u and c3u:
@@ -50,7 +36,7 @@ def _decode_triple(c1, c2, c3):
         raise ValueError('unexpected state: ("' + c1 + '", "' + c2 + '", "' + c3 + '")')
     # Write decoded characters
     if write_break:
-        b += '_'
+        b += "_"
     b += write
     return b
 
@@ -62,7 +48,7 @@ def _decode_camel_case(s):
     last uppercase letter of a sequence is considered the start of a new word
     if it is followed by a lowercase letter.
     """
-    b = ''
+    b = ""
     # c1, c2, and c3 are a sliding window of character triples
     c1 = chr(0)
     _ = c1
