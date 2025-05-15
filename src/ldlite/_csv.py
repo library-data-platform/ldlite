d911888155583fd04c613001496aab3020395f3a
Katherine Bargar <kbargar@fivecolleges.edu>
2025-05-15 18:24:54 +0000

    Run ruff check --fix on _sql.py

--- src/ldlite/_csv.py
+++ src/ldlite/_csv.py
@@ -1,9 +1,8 @@
-from ._sqlx import server_cursor
-from ._sqlx import sqlid
+from ._sqlx import server_cursor, sqlid
 
 
 def _escape_csv(field):
-    b = ''
+    b = ""
     for f in field:
         if f == '"':
             b += '""'
@@ -17,7 +16,7 @@ def _to_csv(db, dbtype, table, filename, header):
     attrs = []
     cur = db.cursor()
     try:
-        cur.execute('SELECT * FROM ' + sqlid(table) + ' LIMIT 1')
+        cur.execute("SELECT * FROM " + sqlid(table) + " LIMIT 1")
         for a in cur.description:
             attrs.append((a[0], a[1]))
     finally:
@@ -25,23 +24,23 @@ def _to_csv(db, dbtype, table, filename, header):
     # Write data
     cur = server_cursor(db, dbtype)
     try:
-        cols = ','.join([sqlid(a[0]) for a in attrs])
-        cur.execute('SELECT ' + cols + ' FROM ' + sqlid(table) + ' ORDER BY ' + ','.join(
+        cols = ",".join([sqlid(a[0]) for a in attrs])
+        cur.execute("SELECT " + cols + " FROM " + sqlid(table) + " ORDER BY " + ",".join(
             [str(i + 1) for i in range(len(attrs))]))
-        fn = filename if '.' in filename else filename + '.csv'
-        with open(fn, 'w') as f:
+        fn = filename if "." in filename else filename + ".csv"
+        with open(fn, "w") as f:
             if header:
-                print(','.join(['"' + a[0] + '"' for a in attrs]), file=f)
+                print(",".join(['"' + a[0] + '"' for a in attrs]), file=f)
             while True:
                 row = cur.fetchone()
                 if row is None:
                     break
-                s = ''
+                s = ""
                 for i, data in enumerate(row):
-                    d = '' if data is None else data
+                    d = "" if data is None else data
                     if i != 0:
-                        s += ','
-                    if attrs[i][1] == 'NUMBER' or attrs[i][1] == 20 or attrs[i][1] == 23:
+                        s += ","
+                    if attrs[i][1] == "NUMBER" or attrs[i][1] == 20 or attrs[i][1] == 23:
                         s += str(d)
                     else:
                         s += '"' + _escape_csv(str(d)) + '"'
