155fdf032173e595d8c8596c9be5a04808151f51
Katherine Bargar <kbargar@fivecolleges.edu>
2025-05-15 18:14:32 +0000

    Run ruff check --fix

--- src/ldlite/_sqlx.py
+++ src/ldlite/_sqlx.py
@@ -10,13 +10,12 @@ class _DBType(Enum):
 
 
 def _strip_schema(table):
-    st = table.split('.')
+    st = table.split(".")
     if len(st) == 1:
         return table
-    elif len(st) == 2:
+    if len(st) == 2:
         return st[1]
-    else:
-        raise ValueError('invalid table name: ' + table)
+    raise ValueError("invalid table name: " + table)
 
 
 def _autocommit(db, dbtype, enable):
@@ -33,82 +32,77 @@ def _autocommit(db, dbtype, enable):
 
 def _server_cursor(db, dbtype):
     if dbtype == _DBType.POSTGRES:
-        return db.cursor(name=('ldlite' + secrets.token_hex(4)))
-    else:
-        return db.cursor()
+        return db.cursor(name=("ldlite" + secrets.token_hex(4)))
+    return db.cursor()
 
 
 def _sqlid(ident):
-    sp = ident.split('.')
+    sp = ident.split(".")
     if len(sp) == 1:
         return '"' + ident + '"'
-    else:
-        return '.'.join(['"' + s + '"' for s in sp])
+    return ".".join(['"' + s + '"' for s in sp])
 
 
 def _cast_to_varchar(ident: str, dbtype: _DBType):
     if dbtype == _DBType.SQLITE:
-        return 'CAST(' + ident + ' as TEXT)'
-    return ident + '::varchar'
+        return "CAST(" + ident + " as TEXT)"
+    return ident + "::varchar"
 
 
 def _varchar_type(dbtype):
     if dbtype == _DBType.POSTGRES or _DBType.SQLITE:
-        return 'text'
-    else:
-        return 'varchar'
+        return "text"
+    return "varchar"
 
 
 def _json_type(dbtype):
     if dbtype == _DBType.POSTGRES:
-        return 'jsonb'
-    elif dbtype == _DBType.SQLITE:
-        return 'text'
-    else:
-        return 'varchar'
+        return "jsonb"
+    if dbtype == _DBType.SQLITE:
+        return "text"
+    return "varchar"
 
 
 def _encode_sql_str(dbtype, s):
     if dbtype == _DBType.POSTGRES:
-        b = 'E\''
+        b = "E'"
     else:
-        b = '\''
+        b = "'"
     if dbtype == _DBType.SQLITE or dbtype == _DBType.DUCKDB:
         for c in s:
-            if c == '\'':
-                b += '\'\''
+            if c == "'":
+                b += "''"
             else:
                 b += c
     if dbtype == _DBType.POSTGRES:
         for c in s:
-            if c == '\'':
-                b += '\'\''
-            elif c == '\\':
-                b += '\\\\'
-            elif c == '\n':
-                b += '\\n'
-            elif c == '\r':
-                b += '\\r'
-            elif c == '\t':
-                b += '\\t'
-            elif c == '\f':
-                b += '\\f'
-            elif c == '\b':
-                b += '\\b'
+            if c == "'":
+                b += "''"
+            elif c == "\\":
+                b += "\\\\"
+            elif c == "\n":
+                b += "\\n"
+            elif c == "\r":
+                b += "\\r"
+            elif c == "\t":
+                b += "\\t"
+            elif c == "\f":
+                b += "\\f"
+            elif c == "\b":
+                b += "\\b"
             else:
                 b += c
-    b += '\''
+    b += "'"
     return b
 
 
 def _encode_sql(dbtype, data):
     if data is None:
-        return 'NULL'
-    elif isinstance(data, str):
+        return "NULL"
+    if isinstance(data, str):
         return _encode_sql_str(dbtype, data)
-    elif isinstance(data, int):
+    if isinstance(data, int):
         return str(data)
-    elif isinstance(data, bool):
-        return 'TRUE' if data else 'FALSE'
-    else:
-        return _encode_sql_str(dbtype, str(data))
+    if isinstance(data, bool):
+        return "TRUE" if data else "FALSE"
+    return _encode_sql_str(dbtype, str(data))
