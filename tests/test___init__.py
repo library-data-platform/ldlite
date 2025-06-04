455b34715da0ee61a13446342bc4184e38e282ac
Katherine Bargar <kbargar@fivecolleges.edu>
2025-06-04 13:34:02 +0000

    Add tests for the folio connection

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -1,17 +1,64 @@
-from unittest import TestCase
+from dataclasses import astuple, dataclass
 
-from ldlite import LDLite
+import pytest
+from pytest_cases import parametrize_with_cases
+from requests import exceptions
 
 
-class TestLDLite(TestCase):
-    def test_connect_db(self) -> None:
-        ld = LDLite()
-        ld.connect_folio(
-            url="https://folio-etesting-snapshot-kong.ci.folio.org",
-            tenant="diku",
-            user="diku_admin",
-            password="admin",
+def test_ok() -> None:
+    from ldlite import LDLite as uut
+
+    ld = uut()
+    ld.connect_folio(
+        url="https://folio-etesting-snapshot-kong.ci.folio.org",
+        tenant="diku",
+        user="diku_admin",
+        password="admin",
+    )
+    ld.connect_db()
+    ld.query(table="g", path="/groups", query="cql.allRecords=1 sortby id")
+    ld.select(table="g__t")
+
+
+@dataclass(frozen=True)
+class FolioConnectionCase:
+    expected: type[Exception]
+    url: str = "https://folio-etesting-snapshot-kong.ci.folio.org"
+    tenant: str = "diku"
+    user: str = "diku_admin"
+    password: str = "admin"
+
+
+class FolioConnectionCases:
+    def case_url(self) -> FolioConnectionCase:
+        return FolioConnectionCase(
+            expected=exceptions.RequestException,
+            url="https://not.folio.fivecolleges.edu",
         )
-        ld.connect_db()
-        ld.query(table="g", path="/groups", query="cql.allRecords=1 sortby id")
-        ld.select(table="g__t")
+
+    def case_tenant(self) -> FolioConnectionCase:
+        return FolioConnectionCase(
+            expected=RuntimeError,
+            tenant="not a tenant",
+        )
+
+    def case_user(self) -> FolioConnectionCase:
+        return FolioConnectionCase(
+            expected=RuntimeError,
+            user="not a user",
+        )
+
+    def case_password(self) -> FolioConnectionCase:
+        return FolioConnectionCase(
+            expected=RuntimeError,
+            password="",
+        )
+
+
+@parametrize_with_cases("tc", cases=FolioConnectionCases)
+def test_bad_folio_connection(tc: FolioConnectionCase) -> None:
+    from ldlite import LDLite as uut
+
+    ld = uut()
+    with pytest.raises(tc.expected):
+        ld.connect_folio(*astuple(tc)[1:])
