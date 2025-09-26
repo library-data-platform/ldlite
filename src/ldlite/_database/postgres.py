38fcdcf63562b72ba37ec6ead8cb593681d6d870
Katherine Bargar <kbargar@fivecolleges.edu>
2025-09-26 19:22:26 +0000

    Less complex regex for postgres datetime check

--- src/ldlite/_database/postgres.py
+++ src/ldlite/_database/postgres.py
@@ -72,7 +72,7 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION ldlite_system.jis_datetime(j JSONB) RETURNS BOOLEAN AS $$
 BEGIN
     RETURN CASE
-        WHEN ldlite_system.jtype_of(j) = 'string' THEN j->>0 ~ '^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$'
+        WHEN ldlite_system.jtype_of(j) = 'string' THEN j->>0 ~ '^\d{4}-[01]\d-[0123]\dT[012]\d:[012345]\d:[012345]\d\.\d{3}(\+\d{2}:\d{2})?$'
         ELSE FALSE
     END;
 END
